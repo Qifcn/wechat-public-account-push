7ced0e909f160158e13a42258986f7f8b12774c4
Qifcn <34029055+Qifcn@users.noreply.github.com>
2022-09-24 13:31:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,11 +64,11 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oZHU568EYVdqXs37s_nY8-ijTv0w",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "4GJqGnBwLjWuZPpHNq6tItG6iYSmBFqnpzDC4Wh7VF4",
       // 所在省份
-      province: "广东",
+      province: "四川",
       // 所在城市
-      city: "肇庆",
+      city: "成都",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
